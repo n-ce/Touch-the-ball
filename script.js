29a91cb6a434516321d0a617e0f3981fe524d692
n-ce <abrocks1120@gmail.com>
2021-05-02 15:32:32 +0530

    3rd

--- script.js
+++ script.js
@@ -10,5 +10,10 @@ var count = 0;
     else if (count==7){
       h.innerHTML="Level 2";
       b.style.animation = 'move 2s ease-in-out infinite';
+      document.html.style.backgroundColor = 'white';
+    }
+    else if (count==14){
+    h.innerHTML="Level 3";
+    b.style.animationDirection='reverse';
     }
     else{h.innerHTML = count;}}
