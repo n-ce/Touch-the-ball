d7dc79e64913f81047d4bc115d8d7d8a57b7ab79
n-ce <abrocks1120@gmail.com>
2021-05-02 20:36:43 +0530

    added levels

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 var b=document.querySelector('button');
+var w=document.querySelector('html');
 var h=document.querySelector('h1');
 b.addEventListener('click',()=>{document.querySelector('audio').play();});
 
-var count = 0;
+var count = -1;
   b.onclick = function () {
     count++;
-    if (count==5){
-    h.innerHTML="You Won!";}
-    else if (count==7){
+    if (count==0){
+    h.innerHTML="Level 1";
+    b.style.animation='One 2s linear infinite';
+    }
+    else if (count==2){
       h.innerHTML="Level 2";
       b.style.animation = 'move 2s ease-in-out infinite';
-      document.html.style.backgroundColor = 'white';
+      w.style.backgroundImage="url('https://media3.giphy.com/media/l1J9z9csFxtbkGfXG/giphy.webp?cid=6c09b95278a228bc7d33f89f7d94b9984d6acf8e5312a643&rid=giphy.webp&ct=g')";
     }
     else if (count==14){
     h.innerHTML="Level 3";
