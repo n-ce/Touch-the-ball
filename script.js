57f69248593f06434c36e2b0232ba87c8abc27ca
n-ce <abrocks1120@gmail.com>
2021-05-03 09:53:49 +0530

    Fix Viewport overflow

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ var count = -1;
     h.innerHTML="Level 1";
     b.style.animation='One 2s linear infinite';
     }
-    else if (count==2){
+    else if (count==5){
       h.innerHTML="Level 2";
-      b.style.animation = 'move 2s ease-in-out infinite';
-      w.style.backgroundImage="url('https://media3.giphy.com/media/l1J9z9csFxtbkGfXG/giphy.webp?cid=6c09b95278a228bc7d33f89f7d94b9984d6acf8e5312a643&rid=giphy.webp&ct=g')";
+      b.style.animation = 'Two 2s Linear infinite';
+      b.style.backgroundColor='black';
+      w.style.backgroundColor='red';
     }
     else if (count==14){
     h.innerHTML="Level 3";
