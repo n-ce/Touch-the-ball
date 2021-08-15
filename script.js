879dfab20bc7c0bf6539e2bdae60566995b684e3
n-ce <abrocks1120@gmail.com>
2021-08-15 18:19:18 +0530

    v4 : cursive font, removed dependencies, ball dimensions in rem, FINALLY RANDOMIZED TRAJECTORY! , reduced code and many more improvements

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
 var b=document.querySelector('button');
-var w=document.querySelector('html');
-var h=document.querySelector('h3');
-b.addEventListener('click',()=>{document.querySelector('audio').play();});
-
 var count = -1;
-  b.onclick = function () {
-    count++;
-    if (count==0){
-    h.innerHTML="Level 1";
-    b.style.animation='One 2s linear infinite';
-    }
-    else if (count==5){
-      h.innerHTML="Level 2";
-      b.style.animation = 'Two 2s Linear infinite';
-      b.style.backgroundColor='black';
-      w.style.backgroundColor='red';
-    }
-    else if (count==14){
-    h.innerHTML="Level 3";
-    b.style.animationDirection='reverse';
-    }
-    else{h.innerHTML = count;}}
+b.onclick = function () {
+  document.querySelector('audio').play();
+  count++;
+  document.querySelector('h3').innerHTML=count;
+}
+window.onload=(event)=>{
+  setInterval(function(){
+    b.style.transform="translate("+(Math.floor(Math.random()*100)-50)+"vw,"+(Math.floor(Math.random()*100)-50)+"vh)"},750)
+}
